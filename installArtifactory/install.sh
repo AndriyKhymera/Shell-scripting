a32c187d017dee28f65f9f700c8535db72815e97
Andrii Khymera <andriykhymer@gmai.com>
2019-04-09 09:39:02 +0300

    innstall artifactory script

--- installArtifactory/install.sh
+++ installArtifactory/install.sh
@@ -1,29 +1,33 @@
-#! bin/bashartifactory-oss-6.9.0
-
+#!/bin/sh
+echo $OSTYPE
 case "$OSTYPE" in
   solaris*) echo "SOLARIS" ;;
   darwin*)  echo "OSX" ;; 
-  linux*)   
+  linux-gnu)   
 	echo "LINUX" 	
-	curl -L "https://jfrog.bintray.com/artifactory/jfrog-artifactory-oss-6.9.0-sources.tar.gz" -o jfrog-artifactory-oss-6.9.0-sources.tar.gz
+		#curl -L "https://jfrog.bintray.com/artifactory/jfrog-artifactory-oss-6.9.0.zip" -o jfrog-artifactory-oss-6.9.0.zip
+		filename='jfrog-artifactory-oss-6.9.0.zip'
+		outFilename="artifactory-oss-6.9.0"
+		unzip -p $filename > outFilename
+		export ARTIFACTORY_HOME=$outFilename
+		echo $ARTIFACTORY_HOME
+		sh $outFilename/bin/installService.sh
 	;;
   bsd*)     echo "BSD" ;;
   msys*)    
-	echo "WINDOWS" 
-	#curl -L "https://jfrog.bintray.com/artifactory/jfrog-artifactory-oss-6.9.0.zip" -o jfrog-artifactory-oss-6.9.0.zip
-			 #https://bintray.com/jfrog/artifactory/jfrog-artifactory-oss-zip/6.9.0
-	#curl -L "https://bintray.com/jfrog/artifactory-pro/jfrog-artifactory-pro-zip/_latestVersion"
-	#latestURL=$(curl -s -o /dev/null -w "%{redirect_url}" https://bintray.com/jfrog/artifactory/jfrog-artifactory-oss-zip/_latestVersion)
-	#curl -s -o /dev/null -w "%{http_code}" $latestURL
-	#curl -L $latestURL
+		echo "WINDOWS" 
+		#curl -L "https://jfrog.bintray.com/artifactory/jfrog-artifactory-oss-6.9.0.zip" -o jfrog-artifactory-oss-6.9.0.zip
+				#https://bintray.com/jfrog/artifactory/jfrog-artifactory-oss-zip/6.9.0
+		#curl -L "https://bintray.com/jfrog/artifactory-pro/jfrog-artifactory-pro-zip/_latestVersion"
+		#latestURL=$(curl -s -o /dev/null -w "%{redirect_url}" https://bintray.com/jfrog/artifactory/jfrog-artifactory-oss-zip/_latestVersion)
+		#curl -s -o /dev/null -w "%{http_code}" $latestURL
+		#curl -L $latestURL
+		filename='jfrog-artifactory-oss-6.9.0.zip'
+		unzip -q $filename > outFilename
+		echo $outFilename
+		sh "artifactory-oss-6.9.0/artifactory-oss-6.9.0/bin/installService"
 	;;
   *)        echo "unknown: $OSTYPE" ;;
 esac
-
-filename='jfrog-artifactory-oss-6.9.0.zip'
-outFilename='artifactory-oss-6.9.0'
-#unzip -q $filename -d $outFilename
-echo $outFilename
-sh "artifactory-oss-6.9.0/artifactory-oss-6.9.0/bin/installService"
 
 
